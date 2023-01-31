b64fdba7acf4bff32be2100f4494515f53272803
Patryk Matuszak <305846+pmtk@users.noreply.github.com>
2023-01-31 15:21:26 +0100

    change lvms version regexp

--- scripts/auto-rebase/rebase_job_entrypoint.sh
+++ scripts/auto-rebase/rebase_job_entrypoint.sh
@@ -20,10 +20,10 @@ release_arm64="$(oc get configmap/release-release-images-arm64-latest -o yaml \
     | jq -r '.metadata.name')"
 # LVMS is not tracked in the OCP release image.  Instead, rely on the latest from the same X.Y stream as the release image.
 # Input:
-#   registry.ci.openshift.org/ocp/release:4.13.0-0.nightly-2023-01-17-152326
+#   4.13.0-0.nightly-2023-01-17-152326
 # Output:
-#    "4.13"
-release_lvms=$( echo "$release_amd64" | sed -E 's,.*:(v?[0-9]+\.[0-9]+).*,\1,')
+#   4.13
+release_lvms=$( echo "$release_amd64" | sed -E 's,(v?[0-9]+\.[0-9]+).*,\1,')
 
 pullspec_release_amd64="registry.ci.openshift.org/ocp/release:${release_amd64}"
 pullspec_release_arm64="registry.ci.openshift.org/ocp-arm64/release-arm64:${release_arm64}"
