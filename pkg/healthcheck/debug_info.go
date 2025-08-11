54c848b9e501d7c876fca03926824c59f0f243d1
Patryk Matuszak <pmatusza@redhat.com>
2025-08-11 16:01:49 +0200

    Best effort attempt to find unpulled & failed images

--- pkg/healthcheck/debug_info.go
+++ pkg/healthcheck/debug_info.go
@@ -1,12 +1,18 @@
 package healthcheck
 
 import (
+	"context"
+	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/openshift/microshift/pkg/config"
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
+	coreclientv1 "k8s.io/client-go/kubernetes/typed/core/v1"
 	"k8s.io/client-go/util/homedir"
 	"k8s.io/klog/v2"
 	"k8s.io/kubectl/pkg/cmd/get"
@@ -14,7 +20,14 @@ import (
 	"k8s.io/utils/ptr"
 )
 
-func logPodsAndEvents() {
+func postFailureDebugInfo(ctx context.Context, coreClient *coreclientv1.CoreV1Client) {
+	msg := findUnpulledOrFailedImages(ctx, coreClient)
+	msg += logPodsAndEvents()
+
+	klog.Infof("DEBUG INFORMATION\n%s", msg)
+}
+
+func logPodsAndEvents() string {
 	cliOptions := genericclioptions.NewConfigFlags(true).WithDeprecatedPasswordFlag()
 	cliOptions.KubeConfig = ptr.To(filepath.Join(config.DataDir, "resources", string(config.KubeAdmin), "kubeconfig"))
 	if homedir.HomeDir() == "" {
@@ -36,25 +49,133 @@ func logPodsAndEvents() {
 	opts.PrintFlags.OutputFormat = ptr.To("wide")
 	if err := opts.Complete(f, cmdGet, []string{"DUMMY"}); err != nil {
 		klog.Errorf("Failed to complete get's options: %v", err)
-		return
+		return ""
 	}
 
 	if err := opts.Validate(); err != nil {
 		klog.Errorf("Failed to validate get's options: %v", err)
-		return
+		return ""
 	}
 
 	output.WriteString("---------- PODS:\n")
 	if err := opts.Run(f, []string{"pods"}); err != nil {
 		klog.Errorf("Failed to run 'get pods': %v", err)
-		return
+		return ""
 	}
+	output.WriteString("\n")
 	output.WriteString("\n---------- EVENTS:\n")
 	opts.SortBy = ".metadata.creationTimestamp"
 	if err := opts.Run(f, []string{"events"}); err != nil {
 		klog.Errorf("Failed to run 'get events': %v", err)
-		return
+		return ""
+	}
+	output.WriteString("\n")
+	return output.String()
+}
+
+// findUnpulledOrFailedImages makes an attempt to find images that are still being pulled.
+func findUnpulledOrFailedImages(ctx context.Context, coreClient *coreclientv1.CoreV1Client) string {
+	// Get list of existing Pods to skip Events belonging to non-existing Pods to avoid false positives:
+	// If someone creates and deletes a lot of workloads, there might be "Pulling" events for each Pod without
+	// the corresponding "Pulled" event.
+	pods, err := coreClient.Pods("").List(ctx, v1.ListOptions{})
+	if err != nil {
+		klog.Errorf("Failed to retrieve pods: %v", err)
+		return ""
+	}
+	existingPodsNames := sets.New[string]()
+	for _, pod := range pods.Items {
+		existingPodsNames.Insert(pod.Name)
 	}
 
-	klog.Infof("DEBUG INFORMATION\n%s", output.String())
+	var pullingEvents, pulledEvents, failedEvents *corev1.EventList
+	if pullingEvents, err = coreClient.Events("").List(ctx, v1.ListOptions{FieldSelector: "reportingComponent=kubelet,reason=Pulling"}); err != nil {
+		klog.Errorf("Failed to retrieve Pulling events: %v", err)
+		return ""
+	}
+	if pulledEvents, err = coreClient.Events("").List(ctx, v1.ListOptions{FieldSelector: "reportingComponent=kubelet,reason=Pulled"}); err != nil {
+		klog.Errorf("Failed to retrieve Pulled events: %v", err)
+		return ""
+	}
+	if failedEvents, err = coreClient.Events("").List(ctx, v1.ListOptions{FieldSelector: "reportingComponent=kubelet,reason=Failed"}); err != nil {
+		klog.Errorf("Failed to retrieve Failed events: %v", err)
+		return ""
+	}
+
+	unpulledImages, failedImages := analyzeEventsLookingForUnpulledOrFailedImages(existingPodsNames, pullingEvents, pulledEvents, failedEvents)
+
+	output := strings.Builder{}
+
+	if len(unpulledImages) > 0 {
+		output.WriteString("---------- IMAGES THAT ARE STILL BEING PULLED:\n")
+		for _, unpulledImage := range unpulledImages {
+			output.WriteString(fmt.Sprintf("- %q for Pod %q in namespace %q\n", unpulledImage.Image, unpulledImage.PodName, unpulledImage.Namespace))
+		}
+		output.WriteString("\n")
+	}
+
+	if len(failedImages) > 0 {
+		output.WriteString("---------- IMAGES THAT FAILED TO BE PULLED:\n")
+		for _, failedImage := range failedImages {
+			output.WriteString(fmt.Sprintf("- %q for Pod %q in namespace %q: %s\n", failedImage.Image, failedImage.PodName, failedImage.Namespace, failedImage.Message))
+		}
+		output.WriteString("\n")
+	}
+
+	return output.String()
+}
+
+type unpulledImage struct {
+	Namespace string
+	PodName   string
+	Image     string
+}
+
+type failedImage struct {
+	unpulledImage
+	Message string
+}
+
+func analyzeEventsLookingForUnpulledOrFailedImages(existingPodsNames sets.Set[string], pullingEvents, pulledEvents, failedEvents *corev1.EventList) ([]unpulledImage, []failedImage) {
+	getImageInfo := func(event corev1.Event) (string, string, string) {
+		pod := event.InvolvedObject.Name
+		ns := event.InvolvedObject.Namespace
+		img := strings.Split(event.Message, "\"")[1]
+		return ns, pod, img
+	}
+
+	unpulledImages := sets.New[unpulledImage]()
+
+	for _, event := range pullingEvents.Items {
+		ns, pod, img := getImageInfo(event)
+		if !existingPodsNames.Has(pod) {
+			continue
+		}
+		unpulledImages.Insert(unpulledImage{Namespace: ns, PodName: pod, Image: img})
+	}
+
+	for _, event := range pulledEvents.Items {
+		ns, pod, img := getImageInfo(event)
+		unpulledImages.Delete(unpulledImage{Namespace: ns, PodName: pod, Image: img})
+	}
+
+	failedImages := sets.New[failedImage]()
+
+	for _, event := range failedEvents.Items {
+		if !strings.HasPrefix(event.Message, "Failed to pull image") {
+			continue
+		}
+		ns, pod, img := getImageInfo(event)
+		if !existingPodsNames.Has(pod) {
+			continue
+		}
+		unpulledImages.Delete(unpulledImage{Namespace: ns, PodName: pod, Image: img})
+
+		failedImages.Insert(failedImage{
+			unpulledImage: unpulledImage{Namespace: ns, PodName: pod, Image: img},
+			Message:       event.Message,
+		})
+	}
+
+	return unpulledImages.UnsortedList(), failedImages.UnsortedList()
 }
